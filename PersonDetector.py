a4c3d208568531f91970dafc456ff50b014ba855
PRATYUSH KUMAR <pratyushk2011@gmail.com>
2021-09-26 01:59:52 +0530

    Add files via upload

---
+++ PersonDetector.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import keras
+import tensorflow_hub as hub
+import cv2
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def person_detector(input_image, prob_threshold = 0.5):
+    detector = hub.load("https://tfhub.dev/tensorflow/ssd_mobilenet_v2/2")
+    image_array = np.asarray(input_image)
+    image_resized = cv2.resize(image_array,(224,224))
+    im1 = tf.expand_dims(image_resized,axis=0)
+    detector_output = detector(im1)
+    predicted_classes = np.array(detector_output["detection_classes"][0])
+    person_indices = np.where(predicted_classes==1)[0]
+    predicted_scores = np.array(detector_output["detection_scores"][0])
+    person_prob = [predicted_scores[i] for i in person_indices]
+    if max(person_prob) > prob_threshold:
+        print('Person Detected')
+        return 1
+    else:
+        print('No Person Detected')
+        return 0
+
+
+
+
---
+++ app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import streamlit as st
+from PersonDetector import person_detector
+from PIL import Image
+st.title("Detecting Humans in scene")
+st.header("Havells Assignment 2")
+st.text("Upload an image")
+
+uploaded_file = st.file_uploader("Choose a jpg file ...", type="jpg")
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='File uploaded.', use_column_width=True)
+    st.write("")
+    st.write("Classifying...")
+    label = person_detector(image)
+    if label == 0:
+        # st.write("No Person Detected")
+        st.markdown(""" <style> .font {
+        font-size:50px ; font-family: 'Cooper Black'; color: #FF9633;} 
+        </style> """, unsafe_allow_html=True)
+        st.markdown('<p class="font">No Person Detected</p>', unsafe_allow_html=True)
+    else:
+        # st.write("Person Detected")
+        st.markdown(""" <style> .font {
+        font-size:50px ; font-family: 'Cooper Black'; color: #FF9633;} 
+        </style> """, unsafe_allow_html=True)
+        st.markdown('<p class="font">Person Detected!</p>', unsafe_allow_html=True)
+
